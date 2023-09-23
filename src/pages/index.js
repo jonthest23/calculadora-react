3bdb88ad11cdbc343a219d2b7e00d4b45147710b
jonthest23 <jsmora@uniboyaca.edu.co>
2023-09-22 21:52:15 -0500

    "arreglado un poco"

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,42 +11,64 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   const [numerouno, setNumerouno] = useState(null);
   const [numerodos, setNumerodos] = useState(null);
-  const operacion = useRef(null);
+  const [resultado, setResultado] = useState(null);
+  const [operacion, setOperacion] = useState(null);
   const numeroactual = useRef(1);
+  const borradoPendiente = useRef(false);
 
   function boton(numero) {
+    if (borradoPendiente.current == true) {
+      borrar();
+      borradoPendiente.current = false;
+    }else
     if (numeroactual.current == 1) {
-      setNumerouno(numerouno + numero);
-    } else {
-      setNumerodos(numerodos + numero);
+      if (numerouno == null) {
+        setNumerouno(numero);
+      }else{
+        setNumerouno(numerouno + numero);
+      }
+    }else if (numeroactual.current == 2) {
+      if (numerodos == null) {
+        setNumerodos(numero);
+      }else{
+        setNumerodos(numerodos + numero);
+      }
     }
   }
   
   function igual() {
-    if (operacion.current == "+") {
-      setNumerouno(numerouno + numerodos);
-      
-    } else if (operacion.current == "-") {
-      setNumerouno(numerouno - numerodos);
-     
-    } else if (operacion.current == "*") {
-      setNumerouno(numerouno * numerodos);
-      
-    } else if (operacion.current == "/") {
-      setNumerouno(numerouno / numerodos);
+    if (operacion == "+") {
+      setResultado(parseInt(numerouno) + parseInt(numerodos));  
+      numeroactual.current = 0;
+      borradoPendiente.current = true;
+    } else if (operacion == "-") {
+      setResultado(parseInt(numerouno) - parseInt(numerodos));
+      numeroactual.current = 0;
+      borradoPendiente.current = true;
+    } else if (operacion == "*") {
+      setResultado(parseInt(numerouno) * parseInt(numerodos));
+      numeroactual.current = 0;
+      borradoPendiente.current = true;
+    } else if (operacion == "/") {
+      setResultado(parseInt(numerouno) / parseInt(numerodos));
+      numeroactual.current = 0;
+      borradoPendiente.current = true;
     }
-    numeroactual.current = 1;
-    borrar();
+    
   
   }
   function borrar() {
     setNumerodos(null);
     setNumerouno(null);
+    setResultado(null);
+    setOperacion(null);
+    numeroactual.current = 1;
   }
   
   function botonfuncionalidad(operacion) {
-    if (!numerouno == null) {
-      operacion.current = operacion;
+    
+    if (!(numerouno == null)) {
+      setOperacion(operacion);
       numeroactual.current = 2;
   }
   }
@@ -62,26 +84,28 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.calculadora}>
           <div className={styles.lbls}>
-            <h2>{numerouno} {operacion.current} {numerodos}</h2>
+            <h2>{numerouno} {operacion}  {numerodos} = {resultado}</h2>
           </div>
           <div className={styles.funcionalidad}>
             <div className={styles.btnsnumeros}>
-              <button onClick={boton(1)} >1</button>
-              <button onClick={boton(2)} >2</button>
-              <button onClick={boton(3)} >3</button>
-              <button onClick={boton(4)} >4</button>
-              <button onClick={boton(5)} >5</button>
-              <button onClick={boton(6)} >6</button>
-              <button onClick={boton(7)} >7</button>
-              <button onClick={boton(8)} >8</button>
-              <button onClick={boton(9)} >9</button>
+              <button onClick= {()=> boton('1')} >1</button>
+              <button onClick= {()=> boton('2')} >2</button>
+              <button onClick= {()=> boton('3')} >3</button>
+              <button onClick= {()=> boton('4')} >4</button>
+              <button onClick= {()=> boton('5')} >5</button>
+              <button onClick= {()=> boton('6')} >6</button>
+              <button onClick= {()=> boton('7')} >7</button>
+              <button onClick= {()=> boton('8')} >8</button>
+              <button onClick= {()=> boton('9')} >9</button>
+              <button onClick= {()=> boton('0')} >0</button>
+            
             </div>
             <div className={styles.btns}>
               <button onClick ={borrar}>C</button>
-              <button onClick={botonfuncionalidad("+")}>+</button>
-              <button onClick={botonfuncionalidad("-")}>-</button>
-              <button onClick={botonfuncionalidad("*")}>*</button>
-              <button onClick={botonfuncionalidad("/")}>/</button>
+              <button onClick={()=> botonfuncionalidad("+")}>+</button>
+              <button onClick={()=> botonfuncionalidad("-")}>-</button>
+              <button onClick={()=> botonfuncionalidad("*")}>*</button>
+              <button onClick={()=> botonfuncionalidad("/")}>/</button>
               <button onClick={igual}>=</button>
             </div>
           </div>
